2c06ff5f002e2bcb42f745e058158ddce737209a
github username <github email address>
2020-06-11 11:25:58 -0500

    sets up sessions, sessionscontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < Sinatra::Base
     set :views, -> {File.join(root, "../views")}
+    enable :sessions
+    set :session_secret, ENV['SESSION_SECRET']
 
 
     get '/' do 
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,15 @@ i3 = Item.create(name: "ketchup", expy_date: "7-8-22")
 f = Fridge.create(user: u)
 l = List.create(user: u)
 
+fridge_items = Fridge.create! fields: 'values'
+fridge_items.items.create! fields: 'values'
 
+list_items = List.create! fields: 'values'
+list_items.items.create! fields: 'values'
+
+
+#@fridge = Fridge.find_or_create_by x: "y"
+#@fridge  = @fridge.items.find_or_create_by file: x
+
+#@list = List.find_or_create_by x: "y"
+#@list.items << @items
