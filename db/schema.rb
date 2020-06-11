d60491da9f1138160c66966f5372ea97c73903a2
github username <github email address>
2020-06-10 21:26:44 -0500

    starts seed file, need to add itemizable pieces

--- db/schema.rb
+++ db/schema.rb
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2020_06_10_192534) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "username"
+    t.string "email"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+u = User.create(name: "Jordan", username: "Jordles", email: "jordles@gmail.com", password_digest: "1234")
+
+i = Item.create(name: "apple", expy_date: "6-11-20")
+i2 = Item.create(name: "banana", expy_date: "6-12-20")
+i3 = Item.create(name: "ketchup", expy_date: "7-8-22")
+f = Fridge.create(user: u)
+l = List.create(user: u)
+
+
