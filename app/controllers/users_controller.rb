32b8b7831ee4f62319a1aa5371a8b73da242db7a
github username <github email address>
2020-06-19 16:55:18 -0500

    adds password validation, adds account edit functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,42 @@ class UsersController < ApplicationController
 
     get '/dashboard' do 
         authenticate
-        erb :'user/dashboard'
+        erb :'users/dashboard'
     end 
+
+    get '/account/edit' do 
+        authenticate
+        erb :'users/edit_account'
+    end 
+
+    patch '/account' do 
+        authenticate
+        @user = current_user
+        if !@user.authenticate(params[:password])
+            @user.errors.add(:password, "was incorrect")
+            erb :'users/edit_account'
+        else
+            
+            if params[:new_password].empty? 
+                @user.update(username: sanitize(params[:username]))
+                if @user.errors.any? 
+                    erb :'users/edit_account'
+               else
+                   redirect '/dashboard'
+                end
+             
+            else
+                @user.update(username: sanitize(params[:username]), password: sanitize(params[:new_password]), password_confirmation: sanitize(params[:new_password_confirmation]))
+                if @user.errors.any? 
+                    erb :'users/edit_account'
+                else
+                    redirect '/dashboard'
+                end
+        
+            end
+        end 
+    end 
+
 
 
 end 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,11 @@
 class User < ActiveRecord::Base 
+    validates :username, uniqueness: true
+    validates :password, length: {in: 6..20}, unless: ->(u){ u.password.blank? } 
+    has_secure_password
+
     has_one :fridge, dependent: :destroy
     has_one :list, dependent: :destroy
     has_many :fridge_items, through: :fridge, source: :items
     has_many :list_items, through: :list, source: :items 
-    has_secure_password
-    validates :username, uniqueness: true
 
-    def slug
-        self.username.downcase.tr("","-")
-      end 
-    
-      def self.find_by_slug(slug)
-        self.all.find do |instance|
-          instance.slug == slug
-        end 
-     
-      
-    end
 end 
