7d94e66f673f2802beb0210eff97b761bf23c9ad
github username <github email address>
2020-06-26 15:04:41 -0500

    comments out unused code

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,21 +1,21 @@
 class Item < ActiveRecord::Base 
     belongs_to :itemizable, polymorphic: true
 
-    def from_fridge?
-        self.itemizable.is_a?(Fridge)
-    end
+    # def from_fridge?
+    #     self.itemizable.is_a?(Fridge)
+    # end
 
-    def fridge
-        self.itemizable if from_fridge?
-    end
+    # def fridge
+    #     self.itemizable if from_fridge?
+    # end
 
-    def list
-        self.itemizable if from_list?
-    end
+    # def list
+    #     self.itemizable if from_list?
+    # end
 
-    def from_list?
-        self.itemizable.is_a?(List)
-    end
+    # def from_list?
+    #     self.itemizable.is_a?(List)
+    # end
 
     
 end 
